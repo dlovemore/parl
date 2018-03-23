330fe1fdcb9567a193a77605479a4f6a4fb8f467
David Lovemore <dl@ravenbrook.com>
2018-03-23 15:15:17 +0000

    Remove sketch stuff, add tag as tag.

--- util.py
+++ util.py
@@ -2,10 +2,10 @@ def taggedtuple(tag, names=None):
     _n = names and len(names)
     _names = names # to provide access to names if given
     def initial_dict(names):
-        d=dict(__new__=_new_, __init__=_init_, __repr__=_repr_, _tag=tag)
+        d=dict(__new__=_new_, __init__=_init_, __repr__=_repr_, _tag=tag, tag=tag)
         if names:
-            assert(len(names)==len(set(names)))
             for (i, name) in enumerate(names):
+                if name in d: raise ValueError(f'Attr {name} already exists')
                 if name:
                     d[name] = property((lambda i: lambda self: self[i])(i))
         return d
