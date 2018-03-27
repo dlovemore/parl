6fc70408a60e4a1dd1444e54614a6ca3ab416980
David Lovemore <dl@ravenbrook.com>
2018-03-27 14:30:41 +0100

    Experimenting with lisp style symbol hack.

--- symbol.py
+++ symbol.py
@@ -1,14 +1,26 @@
 class Symbol:
     symbols = {}
     def __new__(cls, name):
-        if name in Symbol.symbols: return Symbol.symbols[name]
+        if name in cls.symbols: return cls.symbols[name]
         return super(Symbol, cls).__new__(cls)
     def __init__(self, name):
-        if name in self.symbols:
+        if name in self.__class__.symbols:
             assert(self.name==name)
             return
         self.name = name
-        self.symbols[name] = self
+        self.__class__.symbols[name] = self
         if name not in globals():
             globals()[name]=self
-    def __repr__(self): return self.name
+    def __getattr__(self, attr):
+        return Tuple((self, Symbol(attr),))
+    def __repr__(self):
+        return self.name
+
+class Tuple(tuple):
+    def __new__(cls, symbols):
+        return super().__new__(cls, symbols)
+    def __getattr__(self, attr):
+        return Tuple(self + ((Symbol(attr)),))
+
+class MySymbol(Symbol):
+    symbols={}
